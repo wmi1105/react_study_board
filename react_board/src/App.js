e477d4291c3bc49cddb54fb3e1836f9d92268e7c
dkcla7788@gmail.com <dkcla7788@gmail.com>
2020-12-16 18:18:23 +0900

    react x node js

--- react_board/src/App.js
+++ react_board/src/App.js
@@ -1,25 +1,39 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    title : null
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3002/api')
+    .then(res => res.json())
+    .then(data => this.setState({title: data.greeting}))
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div>
+            {/* Edit <code>src/App.js</code> and save to reload. */}
+            {this.state.title? <h1>{this.state.title}</h1>:<h1>loading...</h1>}
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
